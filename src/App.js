f69a9dc11fd547a1f58b6bddba956ea165c23613
abhayjat <abhay.jat@outlook.com>
2021-01-18 21:57:50 +0530

    Add indec.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      App
-    </div>
+    <Router>
+      <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      </div>
+    </Router>
+    
   );
 }
 

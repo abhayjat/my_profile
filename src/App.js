cb3073f9428d7aa5bc3a229fc072c1d40263d51b
abhayjat <abhay.jat@outlook.com>
2021-01-19 14:43:27 +0530

    Add About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import About from './components/About'; 
 import './App.css';
 
 function App() {
@@ -9,12 +10,13 @@ function App() {
       <div className="app">
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
       </Switch>
     <Footer/>
       </div>
     </Router>
     
   );
-}
+};
 
 export default App;

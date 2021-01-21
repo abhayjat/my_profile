8695143c5daf86bf44dfe28ac8c9701cdea9607f
abhayjat <abhay.jat@outlook.com>
2021-01-21 13:06:09 +0530

    Added Skills, Contect

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import About from './components/About'; 
+import Navbar from './components/NavBar';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
+        <Route exact path="/skills" component={Skills}/>
+        <Route exact path="/contact" component={Contact}/>
       </Switch>
     <Footer/>
       </div>
